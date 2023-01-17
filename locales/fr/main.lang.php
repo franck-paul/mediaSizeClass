c16bee7b657672fa03e25e36dccdb1160e29f4d0
franck <carnet.franck.paul@gmail.com>
2023-01-17 12:24:46 +0100

    Post-commit hook: minify+l18n

---
+++ locales/fr/main.lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+l10n::$locales['Add CSS classes to images from your public folder'] = 'Ajoutez des classes CSS aux images en provenance de votre répertoire de médias';
+l10n::$locales['mediaSizeClass'] = '';
+l10n::$locales['Add CSS classes to your medias'] = 'Ajoutez des classes CSS à vos images';
+l10n::$locales['Add a CSS class in &lt;img /&gt; tag depending on media size inserted: "thumbnail-img" to thumbnail-size medias; "square-img" to square-size medias; "small-img" to small-size medias; "medium-img" to medium-size medias.'] = 'Ajoute dans la balise &lt;img /&gt; des images en provenance de votre répertoire de médias les classes "thumbnail-img", "square-img", "small-img", "medium-img" en fonction du format de l\'image insérée.';
